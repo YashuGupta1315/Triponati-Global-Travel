f2aba7bc6ca2489fd7fc699c13895678671b343e
Yashu <152513866+YashuGupta1315@users.noreply.github.com>
2024-08-09 11:16:20 +0000

    india states added

---
+++ src/components/IndiaData.js
@@ -0,0 +1,15 @@
+import "./IndiaStyle.css";
+function IndiaData(props) {
+  return (
+    <div className="I-card">
+      <div className="I-image">
+        <img src={props.Img} alt={props.Alt} />
+      </div>
+      <h4>{props.header}</h4>
+      <a href={props.Url} target="_blank">
+        {props.visit}
+      </a>
+    </div>
+  );
+}
+export default IndiaData;
