99d8009623517fa3da0235151b7295d14dfd770b
yashu gupta <yashugupta1315@gamil.com>
2024-08-17 16:54:11 +0530

    footer link

--- src/components/India.js
+++ src/components/India.js
@@ -60,7 +60,7 @@ function India() {
             Img="https://images.unsplash.com/photo-1621392652239-dc083d01942d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             Alt=" Gujarat"
             header=" Gujarat"
-            Url="/"
+            Url="/Gujarat"
             visit="Visit"
           />
           <IndiaData
