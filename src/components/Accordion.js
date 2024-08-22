57ef07192294b6f5be27a9c93dda57f8edd3c4ca
YashuGupta1315 <152513866+YashuGupta1315@users.noreply.github.com>
2024-08-22 06:02:16 +0000

    changes in css

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,41 +6,45 @@ const Accordion = ({ title, content, Max, MaxAltitude, MaxAlt, Over, OverNight, 
     setIsOpen(!isOpen);
   };
   return (
+    <>
+    <div className="iternary">
+          <h1>Outline Itinerary </h1>
+    </div>
     <div className="accordion-item">
-      <div className="accordion-title" onClick={toggleAccordion}>
-        <h3>{title}</h3>
-        <span>{isOpen ? "-" : "+"}</span>
-      </div>
-      {isOpen && (
-        <div className="accordion-content">
-          <p>{content}</p>
-          <div className="main">
-            <div className="container">
-              <div className="box">
-                <i className={Max}></i>
-                <p>{MaxAltitude}</p>
-                <p>{MaxAlt}</p>
-              </div>
-              <div className="box">
-                <i className={Over}></i>
-                <p>{OverNight}</p>
-                <p>{hotel}</p>
-              </div>
-              <div className="box">
-                <i className={meal}></i>
-                <p>{Meal}</p>
-                <p>{MealDetail}</p>
-              </div>
-              <div className="box">
-                <i className={Time}></i>
-                <p>{TimeDetail}</p>
-                <p>{dura}</p>
+        <div className="accordion-title" onClick={toggleAccordion}>
+          <h3>{title}</h3>
+          <span>{isOpen ? "-" : "+"}</span>
+        </div>
+        {isOpen && (
+          <div className="accordion-content">
+            <p>{content}</p>
+            <div className="main">
+              <div className="container">
+                <div className="box">
+                  <i className={Max}></i>
+                  <p>{MaxAltitude}</p>
+                  <p>{MaxAlt}</p>
+                </div>
+                <div className="box">
+                  <i className={Over}></i>
+                  <p>{OverNight}</p>
+                  <p>{hotel}</p>
+                </div>
+                <div className="box">
+                  <i className={meal}></i>
+                  <p>{Meal}</p>
+                  <p>{MealDetail}</p>
+                </div>
+                <div className="box">
+                  <i className={Time}></i>
+                  <p>{TimeDetail}</p>
+                  <p>{dura}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div></>
   );
 };
 export default Accordion;
