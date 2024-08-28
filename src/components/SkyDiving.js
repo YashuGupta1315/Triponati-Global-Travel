410d793e58e6f2900d6355c8265b30986730ef98
yashu gupta <yashugupta1315@gamil.com>
2024-08-28 18:15:26 +0530

    skyy

--- src/components/SkyDiving.js
+++ src/components/SkyDiving.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ImageGallery from "./ImageGallery";
-import Accordion from "./Accordion";
+import AccordionAdvAdv from "./AccordionAdvAdv";
 import StickyForm from "./StickyForm";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import AdventureDetail from "./AdventureDetail";
+import AccordionAdv from "./AccordionAdv";
 const images = [
     {
       src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR83VeE04zqC9MUtezEoVa96hLId0GKDcZ_LQ&s",
@@ -15,7 +16,7 @@ const images = [
         alt: "Pokhara",
     },
     {
-        src: "https://pbs.twimg.com/media/CGYlNnPUQAAVlwx.jpg",
+        src: "https://www.natureloverstrek.com/pagegallery/everything-you-need-to-know-about-skydiving-in-pokhara12.jpg",
         alt: "Pokhara",
     },
     {
@@ -48,24 +49,29 @@ const SkyDiving = () =>{
             Detail5="Tourist Class Hotels"
             main6="Drop zone Altitude"
             Detail6="820m (2689ft)"
-
+            head="Highlights"
+            li1="Thrilling skydiving experience with views of the Himalayas, including Annapurna and Machhapuchhre."
+            li2="Jumps from 12,000 to 15,000 feet with a 40-60 second freefall."
+            li3="Stunning scenery of snow-capped mountains, green valleys, and Phewa Lake."
+            li4="High safety standards with professional tandem instructors"
+            li5="Best time to visit: Autumn (October-November) and Spring (March-April)."
             />
-            <Accordion title="Day 01: Arrive Kathmandu, Transfer to Hotel"
+            <AccordionAdv title="Day 01: Arrive Kathmandu, Transfer to Hotel"
             content="You will be welcomed by the Himalayan Shangrila before you land in Kathmandu. Once you land and finish your immigration formalities, you will be received by our representatives and escort you to your hotel."
             />
-            <Accordion title="Day 2: Drive to Kurintar, Rafting in Trishuli and Drive to Pokhara"
+            <AccordionAdv title="Day 2: Drive to Kurintar, Rafting in Trishuli and Drive to Pokhara"
             content="You will take an early drive to Kurintar, which lasts for about 4 hours. Under the guidance of a river guide, you will venture into the Trishuli river and raft along the exciting rapids (rafting past Twin Rock, Ladies Delight and Scout Upset can be a mind-blowing experience). This river journey is found to be electrifying and one of the best ways to explore Nepal. During this meticulously organized river trip, you will journey through some of the unexplored landscapes of Nepal. After the day’s adventure, you will drive to Pokhara."
              />
-             <Accordion title="Day 03 - 06: Jump Day"
+             <AccordionAdv title="Day 03 - 06: Jump Day"
             content="Your jump takes place in one of these days. Your jump schedule will be given after the jump master fixes the schedule during the event. In the excess time, you are free to do your independent activities, like exploring the city. On your jump day, you will be driven to the drop zone in the morning. It takes about 30-40 minutes to reach there. The drop zone is a wide grassland next to a river surrounded by hills. Your whole day will be fun filled as you can also see the lifts and jumps of others apart from yours. In the evening, you will be back to your hotel for your well earned rest."
              />
-            <Accordion title="Day 07: Fly to KTM"
+            <AccordionAdv title="Day 07: Fly to KTM"
             content="A 30 minutes’ flight will bring you to Kathmandu. You can utilize your free time in Kathmandu by strolling down the streets, doing some shopping or enjoying the dusky light of the evening from your hotel."
             />
-            <Accordion title="Day 08: Sightseeing in Kathmandu"
+            <AccordionAdv title="Day 08: Sightseeing in Kathmandu"
             content="Kathmandu, the historical and cultural centre of Nepal, has been a popular destination for tourists ever since Nepal opened its doors to visitors. The city presents a wonderful mix of Hinduism, Tibetan Buddhism and western influence. There will be a guided tour to the UNESCO World heritage Sites Boudhanath -the largest Buddhist Stupa in Nepal, Pashupatinath – the largest Hindu shrine in Nepal dedicated to Lord Shiva, and Patan or Lalitpur, the medieval city of arts and architecture."
             />
-            <Accordion title="Day 09: Depart Kathmandu"
+            <AccordionAdv title="Day 09: Depart Kathmandu"
             content="Our support team will take you to the airport for your flight back home. Or you can stay longer for more exploration activities such as game drive at National parks, trekking, Tibet tours, mountain biking etc. You never know what Nepal has to offer you."
             />
             
