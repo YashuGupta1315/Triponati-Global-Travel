5cf4b668d2d68fe1f1c3a24cf0657f5cd120f1a9
yashu gupta <yashugupta1315@gamil.com>
2024-08-31 19:24:16 +0530

    import css

--- src/components/HeaderSlider.js
+++ src/components/HeaderSlider.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
+import './HeaderSlider.css';
 const HeaderSlider = ({ slides, settings }) => {
  
   const defaultSettings = {
