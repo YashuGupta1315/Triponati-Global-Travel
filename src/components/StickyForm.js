a5e37c8d75a04bc0eb740e9e15c36f346a1e8414
YashuGupta1315 <yashugupta1315@gmail.com>
2024-09-30 12:30:22 +0530

    ..

--- src/components/StickyForm.js
+++ src/components/StickyForm.js
@@ -106,7 +106,7 @@ const StickyForm = ({
               <option value="+376">+376</option>
               <option value="+93">+93</option>
               <option value="+355">+355</option>
-              {/* Add more options as needed */}
+           
             </select>
             <input
               type="tel"
